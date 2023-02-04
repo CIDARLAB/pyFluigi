d3d24b219c303fcb180d290808bbd7ca2d5ac561
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2023-02-04 23:56:58 +0000

    Updated the port spread and the compositecell stuff

--- fluigi/pnr/sfc/port_spread.py
+++ fluigi/pnr/sfc/port_spread.py
@@ -86,7 +86,18 @@ def generate_bin_map(spread_array: List[bool], port_list: List[Port], component:
     return bin_map
 
 
-def shift_furthest_fesible_point(array, start_index, target_index):
+def shift_furthest_fesible_point(array: List[bool], start_index: int, target_index: int) -> None:
+    """ Shift the furthest feasible point to the left or right of the start_index
+    closest to the target_index. This is done by looking at the array and finding
+    the furthest point to the left or right of the start_index and then shifting
+    it.
+
+    Args:
+        array (List[bool]): Spread array representing the side of the component
+        start_index (int): start_index is the index of the point that needs to be shifted
+        target_index (int): target_index is the index of the point to which the start_index needs to be shifted
+    """
+
     # Here the start_index is where the number actually is
     # and the target_index is where the bin_map reckons the
     # the point should be
@@ -101,32 +112,32 @@ def shift_furthest_fesible_point(array, start_index, target_index):
         # loop downwards until you see another
         # for cursor=start_index; cursor>=target_index; i=-1:
         for cursor in range(start_index - 1, target_index - 1, -1):
-            if array[cursor] == 1:
+            if array[cursor] == True:
                 # Found the furthest point set to
                 # right of furthest point and unset the start point
-                array[cursor + 1] = 1
-                array[start_index] = 0
+                array[cursor + 1] = True
+                array[start_index] = False
                 found_flag = True
 
         if found_flag is False:
-            array[target_index] = 1
-            array[start_index] = 0
+            array[target_index] = True
+            array[start_index] = False
 
     elif target_index >= start_index:
         # In the scenario that goes up from start_index -> target_index
         # loop downwards until you see another
         # for cursor=start_index; cursor>=target_index; i=+1:
         for cursor in range(start_index + 1, target_index + 1, 1):
-            if array[cursor] == 1:
+            if array[cursor] == True:
                 # Found the furthest point set to
                 # left of furthest point and unset the start point
-                array[cursor - 1] = 1
-                array[start_index] = 0
+                array[cursor - 1] = True
+                array[start_index] = False
                 found_flag = True
 
             if found_flag is False:
-                array[target_index] = 1
-                array[start_index] = 0
+                array[target_index] = True
+                array[start_index] = False
 
     return
 
